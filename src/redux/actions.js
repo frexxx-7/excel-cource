04826c18be9ea4f79c187d0b767757c478d0aad3
frexxx-7 <borzenkokiril9@gmail.com>
2022-12-28 19:09:16 +0300

    Routing implemented

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,7 +3,8 @@ import {
   CHANGE_STYLES,
   TABLE_RESIZE,
   APPLY_STYLE,
-  CHANGE_TITLE
+  CHANGE_TITLE,
+  UPDATE_DATE
 } from './types'
 
 // Action Creator
@@ -18,6 +19,12 @@ export function changeText(data) {
   return {
     type: CHANGE_TEXT,
     data
+  }
+}
+
+export function updateDate() {
+  return {
+    type: UPDATE_DATE
   }
 }
 
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,7 +3,8 @@ import {
   CHANGE_STYLES,
   TABLE_RESIZE,
   APPLY_STYLE,
-  CHANGE_TITLE
+  CHANGE_TITLE,
+  UPDATE_DATE
 } from './types'
 
 export function rootReducer(state, action) {
@@ -35,6 +36,8 @@ export function rootReducer(state, action) {
       }
     case CHANGE_TITLE:
       return {...state, title: action.data}
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()}
     default: return state
   }
 }
